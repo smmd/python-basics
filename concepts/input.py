eb21873cdc76386e149d69e21ea20dae06c546c4
smeneses <smeneses@etsy.com>
2021-05-19 23:38:10 -0500

    More inputs

---
+++ concepts/input.py
@@ -0,0 +1,16 @@
+print("What is your name?")
+name = input()
+
+print("How old are you?")
+age = int(input())
+
+print("What is your weight?")
+weight = float(input())
+
+print("You are authorized? yes/no")
+authorization = input() == "yes"
+
+print("Hello", name)
+print("Age", age)
+print("Weight", weight)
+print("Authorization", authorization)
